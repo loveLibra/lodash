2a9907f95ab1215bd4b8b6b76790247827e6d332
xuqi <xuqi@i0011.com>
2017-04-02 21:07:30 +0800

    comment on baseSlice

--- .internal/baseSlice.js
+++ .internal/baseSlice.js
@@ -18,6 +18,9 @@ function baseSlice(array, start, end) {
   if (end < 0) {
     end += length
   }
+
+  // >>> 0 make sure the length is a 32-bit unsigned int
+  // link to : http://stackoverflow.com/questions/1822350/what-is-the-javascript-operator-and-how-do-you-use-it
   length = start > end ? 0 : ((end - start) >>> 0)
   start >>>= 0
 
